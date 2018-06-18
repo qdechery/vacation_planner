547a4484942d1d5ea6a7412774007e1e8e98475c
qdechery <qdechery@gmail.com>
2018-06-17 18:11:17 -0700

    added markers, drag markers, delete markers, update location

--- src/app/config-service.service.ts
+++ src/app/config-service.service.ts
@@ -11,7 +11,29 @@ export class ConfigServiceService {
   constructor(private http: HttpClient) { }
 
 
-  getData(loc){
+  getAddress(loc){
   	return this.http.get('https://maps.googleapis.com/maps/api/geocode/json?latlng='+loc+'&key=AIzaSyAXg1EdfqORW9vRMznLUkOzDS79qORUJ8E')
 	}
+
+  getCoord(coordinates){
+	return this.http.get('https://maps.googleapis.com/maps/api/geocode/json?address='+coordinates+'&key=AIzaSyAXg1EdfqORW9vRMznLUkOzDS79qORUJ8E')
+
+  }
+
+  whichPos(positions, event){
+  	for (let i=0; i<positions.length; i++){
+  		if(positions[i].lat() == event.latLng.lat()){
+  			return i
+  		}   		
+  	}
+  }
+
+  whichLoc(locations, event){
+	// console.log(this.getCoord(this.locations[0].results[0].formatted_address));
+  	for (let i=0; i<locations.length; i++){
+  		if(1 == event.latLng.lat()){
+  			return i
+  		}   		
+  	}
+  }
 }
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -12,30 +12,70 @@ import { HttpClient } from '@angular/common/http';
 export class MapComponent {
   positions: any[] = [];
   locations: any[] = [];
-  
-  constructor(private service: ConfigServiceService) { }
+  events: any[] = [];
 
-  
+  constructor(private service: ConfigServiceService) { }
 
   onClick(event) {
   	if(event instanceof MouseEvent) return;
   	this.positions.push(event.latLng);
-  	// console.log(event.latLng.lng());
+  	console.log('Left Click: ', event.latLng.lat(), event.latLng.lng());
  	var newLat = event.latLng.lat()
  	var newLng = event.latLng.lng()
   	var modLatLng = newLat+","+newLng
 
-  	this.service.getData(modLatLng).subscribe(data => this.locations.push(data))
-	// this.locations.push(this.service.getData()
-	// 	.subscribe(data => data)
-	// )  
+  	this.service.getAddress(modLatLng).subscribe(data => {
+
+  		this.locations.push(data)
+  		// console.log(data)
+  	});
+
   }
 
-  // ngOnInit() {
-  // 	this.service.getData().subscribe(data => {
-		// this.locations.push(data.results[1].formatted_address);
-  // 	console.log(this.locations);
-  // }
-  
+  onDrag(event){
+	var newLat = event.latLng.lat()
+ 	var newLng = event.latLng.lng()
+  	var modLatLng = newLat+","+newLng
+
+  	if(event instanceof MouseEvent) return;
+
+  	console.log(event);
+
+  	//1. Get Event lat/lng
+  	var x=event.target.position.lat();
+  	console.log(x);
+
+  	//2. Find the formatted_address
+	// this.service.getAddress(modLatLng).subscribe(data=>{console.log(this.locations.splice(0,1,data.results[0].formatted_address))})
+
+  	//3. Replace loc of locations with results from 2.
+  	
+
+
+  	// if(this.service.getAddress(modLatLng).subscribe(data=>data.results[0].formatted_address) == this.locations[0].results[0].formatted_address){console.log('test')}
+
+
+  	// this.service.getCoord(this.locations[0].results[0].formatted_address).subscribe(data => {
+  		// console.log(data)
+  	// })
+  	// console.log(event.target.position.lat(), event.target.position.lng());
+
+  	// this.locations.splice(this.service.whichLoc(this.locations, event),1,this.service.getAddress(modLatLng).subscribe(data => data));
+
+  	// this.service.getCoord(this.locations[0].results[0].formatted_address).subscribe(data=>{console.log(data)});
+
+  	// for(let i=0; i<this.positions.length; i++) {
+  	// 	if(this.positions[i].lat() == event.latLng.lat() &&
+  	// 		this.positions[i].lng() == event.latLng.lng()){
+  	// 		console.log(i);
+  	// 	}
+  	// }
+
+  }
+
+  onRightClick(event){
+	console.log('Right Click: ', event.latLng.lat(), event.latLng.lng());
+	this.positions.splice(this.service.whichPos(this.positions, event),1)
+  }
 }
 
